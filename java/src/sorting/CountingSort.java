3bbfad5af1ca56eb602ae769cf21ca2e52f8ce51
João Arthur <joaoarthurbm@gmail.com>
2019-11-05 16:53:09 -0300

    CountingSort

---
+++ java/src/sorting/CountingSort.java
@@ -0,0 +1,29 @@
+package sorting;
+
+public class CountingSort {
+
+    public static int[] countingSort(int[] a, int k) {
+    
+        int[] c = new int[k];
+
+        // frequência
+        for (int i = 0; i < a.length; i++) {
+            c[a[i] - 1] += 1;
+        }
+        
+        // cumulativa
+        for (int i = 1; i < c.length; i++) {
+            c[i] += c[i-1];
+        }
+
+        int[] b = new int[a.length];
+
+        for (int i = a.length - 1; i >= 0; i--) {
+            b[c[a[i] - 1] - 1] = a[i];
+            c[a[i] - 1] -= 1;
+        }
+
+        return b;
+    
+    }
+}
