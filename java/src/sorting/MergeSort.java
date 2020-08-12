9662461b2ef56308cff4c6483845d18d56db5da6
João Arthur <joaoarthurbm@gmail.com>
2020-08-12 12:55:53 -0300

    adiciona implementação do merge sort

---
+++ java/src/sorting/MergeSort.java
@@ -0,0 +1,65 @@
+package sorting;
+
+public class MergeSort implements SortingAlgorithm {
+	
+	public void mergeSort(int[] v, int left, int right) {
+		
+		if (left < right) {
+			
+			int middle = (left + right) / 2;
+			mergeSort(v, left, middle);
+			mergeSort(v, middle + 1, right);
+	
+			merge(v, left, middle, right);
+		}
+		
+	}
+	
+	public void merge(int[] v, int left, int middle, int right) {
+		
+		// transfere os elementos entre left e right para o array auxiliar.
+		int[] helper = new int[v.length];
+		for (int i = left; i <= right; i++) {
+			helper[i] = v[i];
+		}
+		
+		
+		int i = left;
+		int j = middle + 1;
+		int k = left;
+		
+		while (i <= middle && j <= right) {
+			
+			if (helper[i] < helper[j]) {
+				v[k] = helper[i];
+				i++;
+			} else {
+				v[k] = helper[j];
+				j++;
+			}
+			k++;	
+			
+		}
+		
+		// se a metade inicial não foi toda consumida, faz o append.
+		while (i <= middle) {
+			v[k] = helper[i];
+			i++;
+			k++;
+		}
+		
+		// se a metade final não foi toda consumida, faz o append.
+		while (j <= right) {
+			v[k] = helper[j];
+			j++;
+			k++;
+		}
+
+	}
+
+	@Override
+	public void sort(int[] v) {
+		mergeSort(v, 0, v.length - 1);
+	}
+
+}
