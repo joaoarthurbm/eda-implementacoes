7fe9151c94842a7e5218e7383c74aeaa0ad12fd9
João Brunet <joaoarthurbm@gmail.com>
2024-03-07 23:11:08 -0300

    Update QuickSort.java

--- java/src/sorting/QuickSort.java
+++ java/src/sorting/QuickSort.java
@@ -2,40 +2,43 @@ package sorting;
 public class QuickSort implements SortingAlgorithm {
 
     private PivotStrategy pivotPicker = new MedianOfThreePivotStrategy();
-    
-	public void quickSort(int[] v, int left, int right) {
-		if (left < right) {
-			int index_pivot = partition(v, left, right);
-			quickSort(v, left, index_pivot - 1);
-			quickSort(v, index_pivot + 1, right);	
-		}
+
+    public void sort(int[] v) {
+        this.quickSort(v, 0, v.length - 1);
+    }
+	
+    public void quickSort(int[] v, int left, int right) {
+        if (left < right) {
+	    int index_pivot = partition(v, left, right);
+	    quickSort(v, left, index_pivot - 1);
+	    quickSort(v, index_pivot + 1, right);	
 	}
+    }
 
-	public int hoarePartition(int[]v, int left, int right) {
-            int pivot = v[left];
-            int i = left + 1;
-            int j = right;
+    public int hoarePartition(int[]v, int left, int right) {
+        int pivot = v[left];
+        int i = left + 1;
+        int j = right;
 
-            while (i <= j) {
-
-                while (i <= j && v[i] <= pivot)
+        while (i <= j) {
+            while (i <= j && v[i] <= pivot)
                     i++;
+            
+	    while (i <= j && v[j] > pivot)
+                j--;
 
-                while (i <= j && v[j] > pivot)
-                    j--;
+             if (i < j) {
+                int aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
+              }
 
-                if (i < j) {
-                    int aux = v[i];
-                    v[i] = v[j];
-                    v[j] = aux;
-                }
+        }
 
-            }
-
-            int aux = v[left];
-            v[left] = v[j];
-            v[j] = aux;
-            return j;
+        int aux = v[left];
+        v[left] = v[j];
+        v[j] = aux;
+        return j;
     }
 
     private int partition(int[] v, int left, int right) {
@@ -66,10 +69,4 @@ public class QuickSort implements SortingAlgorithm {
 	return i;
 		
     }
-
-    @Override
-    public void sort(int[] v) {
-        this.quickSort(v, 0, v.length - 1);
-    }
-
 }
