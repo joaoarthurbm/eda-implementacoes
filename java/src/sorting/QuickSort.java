202ad713623d5cf1cf672558fcf816f333f27ce9
João Brunet <joaoarthurbm@gmail.com>
2024-03-07 23:05:17 -0300

    fix #12

--- java/src/sorting/QuickSort.java
+++ java/src/sorting/QuickSort.java
@@ -11,6 +11,33 @@ public class QuickSort implements SortingAlgorithm {
 		}
 	}
 
+	public int hoarePartition(int[]v, int left, int right) {
+            int pivot = v[left];
+            int i = left + 1;
+            int j = right;
+
+            while (i <= j) {
+
+                while (i <= j && v[i] <= pivot)
+                    i++;
+
+                while (i <= j && v[j] > pivot)
+                    j--;
+
+                if (i < j) {
+                    int aux = v[i];
+                    v[i] = v[j];
+                    v[j] = aux;
+                }
+
+            }
+
+            int aux = v[left];
+            v[left] = v[j];
+            v[j] = aux;
+            return j;
+    }
+
 	private int partition(int[] v, int left, int right) {
 		
 	    int indexPivot = this.pivotPicker.pickPivotIndex(v, left, right);
