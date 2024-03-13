2798e92fbeef7c474e9309ea6b97d73eabfd34c4
Winicius Silva <winiciusab12@gmail.com>
2024-03-13 09:00:32 -0300

    feat: Adiciona implementação para o RadixSort

--- java/src/sorting/RadixSort.java
+++ java/src/sorting/RadixSort.java
@@ -1,32 +1,55 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
- class Radix {
-	
-	public static int[] counting(int[] a, int k, int nthDig) {
-		
-		// frequência
-		int[] c = new int[k+1];
-		for (int i = 0; i < a.length; i++) {
-			int d = (int) (a[i] % Math.pow(10, nthDig));
-			d = (int) (d / Math.pow(10, nthDig - 1));
-			c[d] += 1;
-		}
-		
-		// cumulativa
-		for (int i = 1; i < c.length; i++)
-			c[i] += c[i-1];
-		
-		int[] b = new int[a.length];
-		for (int i = a.length - 1; i >= 0; i--) {
-			int d = (int) (a[i] % Math.pow(10, nthDig));
-			d = (int) (d / Math.pow(10, nthDig - 1));
-			b[c[d] - 1] = a[i];
-			c[d] -= 1;
-		}
-		
-		return b;
-	}
-	
+public class RadixSort {
+
+    public static void radixSort(int[] v) {
+        int max = getMax(v);
+
+        for (int i = 1; max / Math.pow(10, i - 1) > 0; i++) {
+            countingSort(v, i);
+        }
+    }
+
+    private static void countingSort(int[] v, int pos) {
+        int[] a = new int[v.length];
+        int[] c = new int[10];
+
+        for (int i = 0; i < v.length; i++) {
+            int digito = getDigit(v[i], pos);
+            c[digito] += 1;
+        }
+
+        for (int i = 1; i < c.length; i++) {
+            c[i] += c[i - 1];
+        }
+
+        for (int i = v.length - 1; i >= 0; i--) {
+            int digito = getDigit(v[i], pos);
+            a[c[digito] - 1] = v[i];
+            c[digito]--;
+        }
+
+        for (int i = 0; i < v.length; i++) {
+            v[i] = a[i];
+        }
+    }
+
+    private static int getDigit(int N, int i) {
+        return (int) (N / Math.pow(10, i - 1)) % 10;
+    }
+
+    private static int getMax(int[] v) {
+        // Considerando que não vamos receber número inteiros < 0
+        int max = -1;
+       // 
+    
+
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] > max)
+                max = v[i];
+        }
+
+        return max;
+    }
+
 }
-
